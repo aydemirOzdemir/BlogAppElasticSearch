4b8c1f68750139bababb2c27ffb4850b22b1d067
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-04-17 20:48:32 +0300

    Blog Controller oluşturuldu Save actionı ve view eklendi

--- BlogAPP/BlogApp/BlogApp/ViewModels/BlogCreateViewModel.cs
+++ BlogAPP/BlogApp/BlogApp/ViewModels/BlogCreateViewModel.cs
@@ -6,11 +6,13 @@ namespace BlogApp.ViewModels;
 public class BlogCreateViewModel
 {
     [Required]
+    [Display(Name ="Blog Title")]
     public string Title { get; set; } = null!;
     [Required]
+    [Display(Name = "Blog Content")]
     public string Content { get; set; } = null!;
     [Required]
-    public List<string> Tags { get; set; } = new();
- 
-    public Guid UserId { get; set; }
+    [Display(Name = "Blog Tags")]
+    public string? Tags { get; set; } = null!;
+
 }

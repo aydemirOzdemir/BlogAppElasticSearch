28bb4d5af44df6b0c23b164f62249e417ecccd83
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-04-17 19:10:18 +0300

    Elastic client eklendi ve repository yazıldı

---
+++ BlogAPP/BlogApp/BlogApp/Respositories/BlogRepository.cs
@@ -0,0 +1,24 @@
+﻿using BlogApp.Models;
+using Elastic.Clients.Elasticsearch;
+
+namespace BlogApp.Respositories;
+
+public class BlogRepository
+{
+    private readonly ElasticsearchClient client;
+    private const string indexName = "blog";
+
+    public BlogRepository(ElasticsearchClient client)
+    {
+        this.client = client;
+    }
+    public async Task<Blog> SaveAsync(Blog request)
+    {
+        request.Crated = DateTime.Now;
+        IndexResponse result = await client.IndexAsync(request, i => i.Index(indexName));
+        if (!result.IsValidResponse)
+            return null;
+        request.Id=result.Id.ToString();
+        return request;
+    }
+}
